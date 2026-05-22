a6354e4936a209225530db50c0d8520e7da3e774
OwaisMir <owais.mir.dev@gmail.com>
2026-05-22 07:41:01 +0530

    Added json-server to fetch data and implemented loading and error state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Content } from "./components/Content";
 import { Footer } from "./components/Footer";
@@ -7,38 +7,51 @@ import { AddItem } from "./components/AddItem";
 import { SearchItem } from "./components/SearchItem";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("items")));
+  const BASE_URL = "http://localhost:3000";
+
+  const [items, setItems] = useState([]);
   const [itemValue, setItemValue] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getItems() {
+      try {
+        const response = await fetch(`${BASE_URL}/items`);
+        if (!response.ok) throw Error("Failed to get items");
+        const data = await response.json();
+        setItems(data);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    // setTimeout(() => {
+    //   getItems();
+    // }, 2000);
+    getItems();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   function deleteItem(id) {
     const arr = items.filter((item) => item.id !== id);
     setItems(arr);
-    localStorage.setItem("items", JSON.stringify(arr));
   }
   function checkboxHandler(id) {
     const arr = items.map((item) =>
       item.id == id ? { ...item, isAvailable: !item.isAvailable } : item,
     );
     setItems(arr);
-    localStorage.setItem("items", JSON.stringify(arr));
   }
   function handleSubmit(e) {
     e.preventDefault();
     if (!itemValue.trim()) return;
-    if (!items) {
-      const newItems = [
-        {
-          id: 1,
-          name: itemValue,
-          isAvailable: false,
-        },
-      ];
-      setItems(newItems);
-      localStorage.setItem("items", JSON.stringify(newItems));
-      setItemValue("");
-      return;
-    }
 
     const updatedItems = [
       ...items,
@@ -62,14 +75,22 @@ function App() {
         setItemValue={setItemValue}
         handleSubmit={handleSubmit}
       />
-      <Content
-        items={items?.filter((item) =>
-          item.name.toLowerCase().includes(search.toLowerCase()),
+      <main>
+        {loading && <p>Loading items.....</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+
+        {!fetchError && !loading && (
+          <Content
+            items={items?.filter((item) =>
+              item.name.toLowerCase().includes(search.toLowerCase()),
+            )}
+            setItems={setItems}
+            deleteItem={deleteItem}
+            checkboxHandler={checkboxHandler}
+          />
         )}
-        setItems={setItems}
-        deleteItem={deleteItem}
-        checkboxHandler={checkboxHandler}
-      />
+      </main>
+
       <Footer length={items?.length} />
     </div>
   );
